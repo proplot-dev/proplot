92486ced065979abd0de6a36a4e630f282202d0a
Luke Davis <lukelbd@gmail.com>
2018-11-13 16:34:34 -0700

    More units

--- proplot/gridspec.py
+++ proplot/gridspec.py
@@ -11,13 +11,18 @@ def _units(value, error=True):
     # See: http://iamvdo.me/en/blog/css-font-metrics-line-height-and-vertical-align#lets-talk-about-font-size-first
     unit_dict = {
         'em': rc['small']/72.0,
-        'ex': 0.5*rc['small']/72.0, # more or less; see URL
+        'ex': 0.5*rc['large']/72.0, # more or less; see URL
+        'lem': rc['small']/72.0, # for large text
+        'lex': 0.5*rc['large']/72.0,
         'cm': 0.3937,
-        'in': 1.0, # already in inches
         'mm': 0.03937,
         'pt': 1/72.0,
         'l':  1.2*rc['small']/72.0, # the default line spacing for multiline labels
         'lh': 1.2*rc['small']/72.0,
+        'in': 1.0, # already in inches
+        'ft': 12.0, # the rest are included but dunno why you would do this :/
+        'yd': 36.0,
+        'm': 39.37,
         }
     if not isinstance(value, str):
         return value # assume int/float is in inches
