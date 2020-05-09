785bfbf6d93e0cb94213e07518cc82cfc3112cbe
Luke Davis <lukelbd@gmail.com>
2020-05-09 05:05:50 -0600

    Update top-level imports

--- proplot/__init__.py
+++ proplot/__init__.py
@@ -6,20 +6,20 @@ A python package for making beautiful, publication-quality graphics.
 # In the future we will not import the class names
 import pkg_resources as _pkg
 from .config import *  # noqa: F401 F403
-# from .internals import timers
-# with timers._benchmark('imports'):
-from .utils import *  # noqa: F401 F403
-from .crs import *  # noqa: F401 F403
-from .colors import *  # noqa: F401 F403
-from .ticker import *  # noqa: F401 F403
-# from .scale import *  # noqa: F401 F403
-from .gridspec import *  # noqa: F401 F403
-# from .constructor import *  # noqa: F401 F403
-from .wrappers import *  # noqa: F401 F403
-from .axes import *  # noqa: F401 F403
-# from .figure import *  # noqa: F401 F403
-from .ui import *  # noqa: F401 F403
-# from .show import *  # noqa: F401 F403
+from .internals import timers
+with timers._benchmark('imports'):
+    from .utils import *  # noqa: F401 F403
+    from .crs import *  # noqa: F401 F403
+    from .colors import *  # noqa: F401 F403
+    from .ticker import *  # noqa: F401 F403
+    from .scale import *  # noqa: F401 F403
+    from .gridspec import *  # noqa: F401 F403
+    from .constructor import *  # noqa: F401 F403
+    from .wrappers import *  # noqa: F401 F403
+    from .axes import *  # noqa: F401 F403
+    from .figure import *  # noqa: F401 F403
+    from .ui import *  # noqa: F401 F403
+    from .show import *  # noqa: F401 F403
 
 # SCM versioning
 name = 'proplot'
