e099c87d243f73172836f1031cbb2f65d1c674b2
Luke Davis <lukelbd@gmail.com>
2020-06-17 14:06:50 -0600

    Fix issue where global variables in func definition loop overwritten

--- proplot/internals/warnings.py
+++ proplot/internals/warnings.py
@@ -62,7 +62,7 @@ def _rename_objs(version, **kwargs):
     for old_name, func_or_class in kwargs.items():
         new_name = func_or_class.__name__
 
-        def wrapper(*args, **kwargs):
+        def wrapper(*args, old_name=old_name, new_name=new_name, **kwargs):
             _warn_proplot(
                 f'{old_name!r} was deprecated in version {version} and will be '
                 f'removed in the next major release. Please use {new_name!r} instead.'
