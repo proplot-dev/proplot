22c0e352dd84e069ce739b24475dc49e9dce5805
Luke Davis <lukelbd@gmail.com>
2021-07-03 12:44:20 -0600

    Attempt to make redirect page

--- docs/conf.py
+++ docs/conf.py
@@ -175,7 +175,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.html']
 
 # The master toctree document.
 master_doc = 'index'
