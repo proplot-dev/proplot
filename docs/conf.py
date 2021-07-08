89d6f5bd6b9afd5da9c76935fbddbda5bae3391b
Luke Davis <lukelbd@gmail.com>
2021-07-08 08:21:51 -0600

    Update website style

--- docs/conf.py
+++ docs/conf.py
@@ -137,7 +137,7 @@ autoclass_content = 'both'
 # Set up mapping for other projects' docs
 intersphinx_mapping = {
     'cycler': ('https://matplotlib.org/cycler/', None),
-    'matplotlib': ('https://matplotlib.org', None),
+    'matplotlib': ('https://matplotlib.org/stable', None),
     'sphinx': ('http://www.sphinx-doc.org/en/stable', None),
     'python': ('https://docs.python.org/3', None),
     'numpy': ('https://docs.scipy.org/doc/numpy', None),
