87fa3a15d8a0b1516fdb0c20b49710b39c7b635e
Luke Davis <lukelbd@gmail.com>
2021-07-08 20:01:10 -0600

    Always surround docstrings with newlines

--- proplot/internals/docstring.py
+++ proplot/internals/docstring.py
@@ -9,10 +9,12 @@ snippets = {}
 
 
 def add_snippets(func):
-    """Decorator that dedents docstrings with `inspect.getdoc` and adds
+    """
+    Decorator that dedents docstrings with `inspect.getdoc` and adds
     un-indented snippets from the global `snippets` dictionary. This function
     uses ``%(name)s`` substitution rather than `str.format` substitution so
-    that the `snippets` keys can be invalid variable names."""
+    that the `snippets` keys can be invalid variable names.
+    """
     func.__doc__ = inspect.getdoc(func)
     if func.__doc__:
         func.__doc__ %= {key: value.strip() for key, value in snippets.items()}
