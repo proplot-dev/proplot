343b0f7033801cd03815b993a5a5ed0724caf93e
Luke Davis <lukelbd@gmail.com>
2021-08-16 12:31:56 -0600

    Add artist alias translators, helper arg/kwarg processing funcs

--- proplot/internals/__init__.py
+++ proplot/internals/__init__.py
@@ -2,38 +2,45 @@
 """
 Utilities used internally by proplot.
 """
-import matplotlib
+import inspect
 
-from . import docstring, rcsetup, timers, warnings  # noqa: F401
-
-try:
-    import cartopy
-except ImportError:
-    cartopy = None
+from . import benchmarks, dependencies, docstring, rcsetup, warnings  # noqa: F401
+from .dependencies import _version, _version_cartopy, _version_mpl  # noqa: F401
 
 try:  # print debugging
     from icecream import ic
 except ImportError:  # graceful fallback if IceCream isn't installed
     ic = lambda *args: print(*args)  # noqa: E731
 
+INTERNAL_PARAMS = {  # silently pop these if we don't reach certain internal utilities
+    'default_discrete',
+    'default_cmap',
+    'contour_plot',
+    'is_counts',
+    'to_centers',
+    'cycle_manually',
+}
 
-# Aliases. This package only works with a subset of available artists
-# and keywords so we simply create our own system rather than working
-# with matplotlib's normalize_kwargs and _alias_maps.
-ALIASES = {
-    'hsla': {
-        'hue': ('h',),
-        'saturation': ('s', 'c', 'chroma'),
-        'luminance': ('l',),
-        'alpha': ('a',),
-    },
+# Alias dictionaries. This package only works with a subset of available artists
+# and keywords so we simply create our own system rather than working with
+# matplotlib's normalize_kwargs and _alias_maps.
+# WARNING: Add pseudo-props 'edgewidth' and 'fillcolor' for patch edges and faces
+# WARNING: Critical that alias does not appear in key dict or else _translate_kwargs
+# will overwrite settings with None after popping them!
+_alias_dicts = {
     'rgba': {
         'red': ('r',),
         'green': ('g',),
         'blue': ('b',),
         'alpha': ('a',),
     },
-    'lines': {  # copied from lines.py but expanded to include plurals
+    'hsla': {
+        'hue': ('h',),
+        'saturation': ('s', 'c', 'chroma'),
+        'luminance': ('l',),
+        'alpha': ('a',),
+    },
+    'line': {  # copied from lines.py but expanded to include plurals
         'antialiased': ('aa',),
         'alpha': ('a', 'alphas'),
         'color': ('c', 'colors'),
@@ -45,47 +52,43 @@ ALIASES = {
         'markersize': ('s', 'ms', 'markersizes'),
         'markeredgecolor': ('mec', 'markeredgecolors'),
         'markeredgewidth': ('mew', 'markeredgewidths'),
-        'markerfacecolor': ('mfc', 'markerfacecolors'),
+        'markerfacecolor': ('mfc', 'markerfacecolors', 'mc', 'markercolor', 'markercolors'),  # noqa: E501
+        'fillstyle': ('fs', 'fillstyles', 'mfs', 'markerfillstyle', 'markerfillstyles'),
+        'zorder': ('z', 'zorders'),
     },
-    'fills': {
-        'linewidths': ('lw', 'linewidth'),
+    'collection': {  # NOTE: face color is ignored for line collections
+        'alphas': ('a', 'alpha'),
+        'colors': ('c', 'color'),
+        'edgecolors': ('ec', 'edgecolor'),
+        'facecolors': ('fc', 'fillcolor', 'fillcolors'),
+        'linewidths': ('lw', 'linewidth', 'ew', 'edgewidth', 'edgewidths'),
         'linestyles': ('ls', 'linestyle'),
-        'colors': ('c', 'color', 'ec', 'edgecolor', 'edgecolors'),
-    }
+        'zorder': ('z', 'zorders'),
+    },
+    'patch': {
+        'alpha': ('a', 'alphas', 'facealpha', 'facealphas', 'fillalpha', 'fillalphas'),
+        'color': ('c', 'colors'),
+        'edgecolor': ('ec', 'edgecolors'),
+        'facecolor': ('fc', 'facecolors', 'fillcolor', 'fillcolors'),
+        'linewidth': ('lw', 'linewidths', 'ew', 'edgewidth', 'edgewidths'),
+        'linestyle': ('ls', 'linestyles'),
+        'zorder': ('z', 'zorders'),
+        'hatch': ('h', 'hatching'),
+    },
+    'text': {
+        'text': (),
+        'color': ('c', 'fontcolor'),  # NOTE: see text.py source code
+        'fontfamily': ('family',),
+        'fontname': ('name',),
+        'fontsize': ('size',),
+        'fontstretch': ('stretch',),
+        'fontstyle': ('style',),
+        'fontvariant': ('variant',),
+        'fontweight': ('weight',),
+        'fontproperties': ('fp', 'font', 'font_properties'),
+        'zorder': ('z', 'zorders'),
+    },
 }
-
-
-def _getattr_flexible(obj, attr, default=None):
-    """
-    Search for attribute ``attr`` and ``_attr``. This crudely guards against
-    upstream matplotlib changes.
-    """
-    if hasattr(obj, attr) and hasattr(obj, '_' + attr):
-        warnings._warn_proplot(
-            f"Object {obj!r} has both {attr!r} and {'_' + attr!r} attributes."
-            'Using former.'
-        )
-    return getattr(obj, attr, getattr(obj, '_' + attr, default))
-
-
-def _pop_props(kwargs, *categories):
-    """
-    Pop out properties from category `category` after accounting for
-    aliases. Return a dictionary of the non-None property values. This
-    modifies the input `kwargs` dictionary in-place.
-    """
-    props = {}
-    for category in categories:
-        if category not in ALIASES:
-            raise ValueError(f'Invalid alias category {category!r}.')
-        for key, aliases in ALIASES[category].items():
-            if isinstance(aliases, str):
-                aliases = (aliases,)
-            opts = {alias: kwargs.pop(alias, None) for alias in (key, *aliases)}
-            prop = _not_none(**opts)
-            if prop is not None:
-                props[key] = prop
-    return props
 
 
 def _not_none(*args, default=None, **kwargs):
@@ -116,9 +119,117 @@ def _not_none(*args, default=None, **kwargs):
     return first
 
 
-class _dummy_context(object):
+def _keyword_to_positional(options, *args, allow_extra=False, **kwargs):
     """
-    Dummy context manager.
+    Translate keyword arguments to positional arguments. Permit omitted
+    arguments so that plotting functions can infer values.
+    """
+    nargs, nopts = len(args), len(options)
+    if nargs > nopts and not allow_extra:
+        raise ValueError(f'Expected up to {nopts} positional arguments. Got {nargs}.')
+    args = list(args)
+    args.extend(None for _ in range(nopts - nargs))  # fill missing args
+    for idx, keys in enumerate(options):
+        if isinstance(keys, str):
+            keys = (keys,)
+        opts = {}
+        if args[idx] is not None:  # positional args have first priority
+            opts[keys[0] + '_positional'] = args[idx]
+        for key in keys:  # keyword args
+            opts[key] = kwargs.pop(key, None)
+        args[idx] = _not_none(**opts)  # may reassign None
+    return args, kwargs
+
+
+def _translate_kwargs(input, output, *keys, **aliases):
+    """
+    The driver function.
+    """
+    aliases.update({key: () for key in keys})
+    for key, aliases in aliases.items():
+        aliases = (aliases,) if isinstance(aliases, str) else aliases
+        opts = {key: input.pop(key, None) for key in (key, *aliases)}
+        value = _not_none(**opts)
+        if value is not None:
+            output[key] = value
+    return output
+
+
+def _translate_props(input, output, *categories, prefix=None, ignore=None):  # noqa: E501
+    """
+    The driver function.
+    """
+    # Get properties
+    prefix = prefix or ''  # e.g. 'box' for boxlw, boxlinewidth, etc.
+    for category in categories:
+        if category not in _alias_dicts:
+            raise ValueError(f'Invalid alias category {category!r}.')
+        for key, aliases in _alias_dicts[category].items():
+            if isinstance(aliases, str):
+                aliases = (aliases,)
+            opts = {prefix + alias: input.pop(prefix + alias, None) for alias in (key, *aliases)}  # noqa: E501
+            prop = _not_none(**opts)
+            if prop is not None:
+                output[key] = prop
+    # Ignore properties (e.g., ignore 'marker' properties)
+    ignore = ignore or ()
+    if isinstance(ignore, str):
+        ignore = (ignore,)
+    for string in ignore:
+        for key in tuple(output):
+            if string in key:
+                value = output.pop(key)
+                warnings._warn_proplot(f'Ignoring property {key}={value!r}.')
+    return output
+
+
+def _pop_kwargs(src, *keys, **aliases):
+    """
+    Pop out input properties and return them in a new dictionary.
+    """
+    return _translate_kwargs(src, {}, *keys, **aliases)
+
+
+def _process_kwargs(src, *keys, **aliases):
+    """
+    Translate input properties and add translated names to the original dictionary.
+    """
+    return _translate_kwargs(src, src, *keys, **aliases)
+
+
+def _pop_props(src, *categories, **kwargs):
+    """
+    Pop out registered properties and return them in a new dictionary.
+    """
+    return _translate_props(src, {}, *categories, **kwargs)
+
+
+def _process_props(src, *categories, **kwargs):
+    """
+    Translate registered properties and add translated names to the original dictionary.
+    """
+    return _translate_props(src, src, *categories, **kwargs)
+
+
+def _pop_params(kwargs, *funcs, ignore_internal=False):
+    """
+    Pop parameters of the input functions or methods.
+    """
+    output = {}
+    for func in funcs:
+        sig = inspect.signature(func)
+        for key in sig.parameters:
+            value = kwargs.pop(key, None)
+            if ignore_internal and key in INTERNAL_PARAMS:
+                continue
+            if value is not None:
+                output[key] = value
+    return output
+
+
+class _empty_context(object):
+    """
+    A dummy context manager.
     """
     def __init__(self):
         pass
@@ -151,31 +262,3 @@ class _state_context(object):
                 setattr(self._obj, key, self._attrs_prev[key])
             else:
                 delattr(self._obj, key)
-
-
-class _version(list):
-    """
-    Casual version parser for MAJOR.MINOR version strings. Do not want to add
-    'packaging' dependency and only care about major and minor tags.
-    """
-    def __repr__(self):
-        return f'version({self._version})'
-
-    def __init__(self, version):
-        try:
-            major, minor, *_ = version.split('.')
-            major, minor = int(major), int(minor)
-        except (ValueError, AttributeError):
-            warnings._warn_proplot(
-                f"Invalid version {version!r}. Defaulting to '0.0'."
-            )
-            major = minor = 0
-        self._version = version
-        super().__init__([major, minor])  # then use builtin python list sorting
-
-
-_version_mpl = _version(matplotlib.__version__)
-if cartopy is None:
-    _version_cartopy = [0, 0]
-else:
-    _version_cartopy = _version(cartopy.__version__)
