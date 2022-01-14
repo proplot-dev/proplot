2a14a6de9dca497fc732a6fa661ba8640ef60eb7
Luke Davis <lukelbd@gmail.com>
2022-01-13 20:47:52 -0700

    Clean up custom_roles.py

--- docs/sphinxext/custom_roles.py
+++ docs/sphinxext/custom_roles.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python3
+"""
+Custom :rc: and :rcraw: roles for rc settings.
+"""
 from docutils import nodes
 from os.path import sep
 from proplot.internals import rcsetup
 from matplotlib import rcParams
 
 
-def get_nodes(rawtext, text, inliner):
-    repr_ = f"rc['{text}']" if '.' in text else f'rc.{text}'
-    rendered = nodes.Text(repr_)
+def _get_nodes(rawtext, text, inliner):
+    """
+    Return the literal node.
+    """
     source = inliner.document.attributes['source'].replace(sep, '/')
     relsource = source.split('/docs/', 1)
     if len(relsource) == 1:
@@ -15,30 +20,40 @@ def get_nodes(rawtext, text, inliner):
         refuri = 'https://matplotlib.org/stable/tutorials/introductory/customizing.html'
         refuri = f'{refuri}?highlight={text}#the-matplotlibrc-file'
     else:
-        levels = relsource[1].count('/')  # distance to base URL
-        refuri = '../' * levels + 'en/stable/configuration.html'
-        refuri += f'?highlight={text}#table-of-settings'
-    ref = nodes.reference(rawtext, rendered, refuri=refuri)
+        path = '../' * relsource[1].count('/') + 'en/stable'
+        refuri = f'{path}/configuration.html?highlight={text}#table-of-settings'
+    node = nodes.Text(f'rc[{text!r}]' if '.' in text else f'rc.{text}')
+    ref = nodes.reference(rawtext, node, refuri=refuri)
     return [nodes.literal('', '', ref)]
 
 
+def rc_name_role(name, rawtext, text, lineno, inliner, options={}, content=[]):  # noqa: U100, E501
+    """
+    The :rcname: role. Includes a link to the setting.
+    """
+    list_ = _get_nodes(rawtext, text, inliner)
+    return list_, []
+
+
 def rc_role(name, rawtext, text, lineno, inliner, options={}, content=[]):  # noqa: U100
-    node_list = get_nodes(rawtext, text, inliner)
+    """
+    The :rc: role. Includes a link to the setting and its default value.
+    """
+    list_ = _get_nodes(rawtext, text, inliner)
     try:
         default = rcsetup._get_default_param(text)
     except KeyError:
         pass
     else:
-        node_list.append(nodes.Text(' = '))
-        node_list.append(nodes.literal('', '', nodes.Text(repr(default))))
-    return node_list, []
-
-
-def rc_role_raw(name, rawtext, text, lineno, inliner, options={}, content=[]):  # noqa: U100, E501
-    return get_nodes(rawtext, text, inliner), []
+        list_.append(nodes.Text(' = '))
+        list_.append(nodes.literal('', '', nodes.Text(repr(default))))
+    return nodes, []
 
 
 def setup(app):
+    """
+    Set up the roles.
+    """
     app.add_role('rc', rc_role)
-    app.add_role('rcraw', rc_role_raw)
+    app.add_role('rcname', rc_name_role)
     return {'parallel_read_safe': True, 'parallel_write_safe': True}
