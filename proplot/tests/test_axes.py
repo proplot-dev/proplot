b0c78acd0857bfa1e808c38df789f4650d0e85b3
cvanelteren <caspervanelteren@gmail.com>
2024-07-28 07:27:17 +0200

    Unified tests

--- proplot/tests/test_axes.py
+++ proplot/tests/test_axes.py
@@ -3,13 +3,13 @@
 Test twin, inset, and panel axes.
 """
 import numpy as np
-
+import pytest
 import proplot as pplt
 
 state = np.random.RandomState(51423)
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_inset_colors():
     """
     Test color application for zoom boxes.
@@ -17,7 +17,7 @@ def test_inset_colors():
     fig, ax = pplt.subplots()
     ax.format(xlim=(0, 100), ylim=(0, 100))
     ix = ax.inset_axes(
-        (0.5, 0.5, 0.3, 0.3), zoom=True, zoom_kw={'color': 'r', 'fc': 'r', 'ec': 'b'}
+        (0.5, 0.5, 0.3, 0.3), zoom=True, zoom_kw={"color": "r", "fc": "r", "ec": "b"}
     )  # zoom_kw={'alpha': 1})
     # ix = ax.inset_axes((40, 40, 20, 20), zoom=True, transform='data')
     ix.format(xlim=(10, 20), ylim=(10, 20), grid=False)
@@ -25,13 +25,15 @@ def test_inset_colors():
     fig, ax = pplt.subplots()
     ax.format(xlim=(0, 100), ylim=(0, 100))
     ix = ax.inset_axes(
-        (0.3, 0.5, 0.5, 0.3), zoom=True,
-        zoom_kw={'lw': 3, 'ec': 'red9', 'a': 1, 'c': pplt.set_alpha('red4', 0.5)}
+        (0.3, 0.5, 0.5, 0.3),
+        zoom=True,
+        zoom_kw={"lw": 3, "ec": "red9", "a": 1, "c": pplt.set_alpha("red4", 0.5)},
     )
     ix.format(xlim=(10, 20), ylim=(10, 20))
+    return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_inset_zoom_update():
     """
     Test automatic limit adjusment with successive changes. Without the extra
@@ -39,7 +41,7 @@ def test_inset_zoom_update():
     """
     fig, ax = pplt.subplots()
     ax.format(xlim=(0, 100), ylim=(0, 100))
-    ix = ax.inset_axes((40, 40, 20, 20), zoom=True, transform='data')
+    ix = ax.inset_axes((40, 40, 20, 20), zoom=True, transform="data")
     ix.format(xlim=(10, 20), ylim=(10, 20), grid=False)
 
     ix.format(xlim=(10, 20), ylim=(10, 30))
@@ -47,50 +49,55 @@ def test_inset_zoom_update():
 
     ax.format(ylim=(0, 300))
     fig.show()
+    return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_panels_with_sharing():
     """
     Previously the below text would hide the second y label.
     """
     fig, axs = pplt.subplots(ncols=2, share=False, refwidth=1.5)
-    axs.panel('left')
-    fig.format(ylabel='ylabel', xlabel='xlabel')
+    axs.panel("left")
+    fig.format(ylabel="ylabel", xlabel="xlabel")
+    return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_panels_without_sharing():
     """
     What should happen if `share=False` but figure-wide sharing enabled?
     Strange use case but behavior appears "correct."
     """
     fig, axs = pplt.subplots(ncols=2, share=True, refwidth=1.5, includepanels=False)
-    axs.panel('left', share=False)
-    fig.format(ylabel='ylabel', xlabel='xlabel')
+    axs.panel("left", share=False)
+    fig.format(ylabel="ylabel", xlabel="xlabel")
 
     fig, axs = pplt.subplots(ncols=2, refwidth=1.5, includepanels=True)
     for _ in range(3):
-        p = axs[0].panel('l', space=0)
-        p.format(xlabel='label')
-    fig.format(xlabel='xlabel')
+        p = axs[0].panel("l", space=0)
+        p.format(xlabel="label")
+    fig.format(xlabel="xlabel")
+    return fig
 
 
-@pplt.tests.image_compare
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_panels_suplabels():
     """
     Test label sharing for `includepanels=True`.
     """
+    fig = pplt.figure()
     for b in range(3):
         for ncols in range(1, 3):
-            fig, axs = pplt.subplots(ncols=ncols, refwidth=1.5, includepanels=(b == 2))
+            axs = fig.subplots(ncols=ncols, refwidth=1.5, includepanels=(b == 2))
             if b:
                 for _ in range(3):
-                    axs[0].panel('l')
-            axs.format(xlabel='xlabel\nxlabel\nxlabel', ylabel='ylabel', suptitle='sup')
+                    axs[0].panel("l")
+            axs.format(xlabel="xlabel\nxlabel\nxlabel", ylabel="ylabel", suptitle="sup")
+    return fig
 
 
+@pytest.mark.mpl_image_compare
 def test_twin_axes():
     """
     Adjust twin axis positions. Should allow easily switching the location.
@@ -99,30 +106,34 @@ def test_twin_axes():
     fig = pplt.figure()
     ax = fig.subplot()
     ax.format(
-        ycolor='blue', ylabel='orig', ylabelcolor='blue9',
-        yspineloc='l', labelweight='bold', xlabel='xlabel',
-        xtickloc='t', xlabelloc='b',
+        ycolor="blue",
+        ylabel="orig",
+        ylabelcolor="blue9",
+        yspineloc="l",
+        labelweight="bold",
+        xlabel="xlabel",
+        xtickloc="t",
+        xlabelloc="b",
     )
-    ax.alty(
-        loc='r', color='r', labelcolor='red9', label='other', labelweight='bold'
-    )
+    ax.alty(loc="r", color="r", labelcolor="red9", label="other", labelweight="bold")
 
     # Simple example but doesn't quite work. Figure out how to specify left vs. right
     # spines for 'offset' locations... maybe needs another keyword.
     fig, ax = pplt.subplots()
-    ax.format(ymax=10, ylabel='Reference')
-    ax.alty(color='green', label='Green', max=8)
-    ax.alty(color='red', label='Red', max=15, loc=('axes', -0.2))
-    ax.alty(color='blue', label='Blue', max=5, loc=('axes', 1.2), ticklabeldir='out')
+    ax.format(ymax=10, ylabel="Reference")
+    ax.alty(color="green", label="Green", max=8)
+    ax.alty(color="red", label="Red", max=15, loc=("axes", -0.2))
+    ax.alty(color="blue", label="Blue", max=5, loc=("axes", 1.2), ticklabeldir="out")
 
     # A worked example from Riley Brady
     # Uses auto-adjusting limits
     fig, ax = pplt.subplots()
     axs = [ax, ax.twinx(), ax.twinx()]
-    axs[-1].spines['right'].set_position(('axes', 1.2))
-    colors = ('Green', 'Red', 'Blue')
+    axs[-1].spines["right"].set_position(("axes", 1.2))
+    colors = ("Green", "Red", "Blue")
     for ax, color in zip(axs, colors):
         data = state.random(1) * state.random(10)
-        ax.plot(data, marker='o', linestyle='none', color=color)
-        ax.format(ylabel='%s Thing' % color, ycolor=color)
-    axs[0].format(xlabel='xlabel')
+        ax.plot(data, marker="o", linestyle="none", color=color)
+        ax.format(ylabel="%s Thing" % color, ycolor=color)
+    axs[0].format(xlabel="xlabel")
+    return fig
--- proplot/tests/test_projections.py
+++ proplot/tests/test_projections.py
@@ -5,46 +5,49 @@ Test projection features.
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import numpy as np
-
 import proplot as pplt
+import pytest
 
 state = np.random.RandomState(51423)
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_aspect_ratios():
     """
     Test aspect ratio adjustments.
     """
     fig, axs = pplt.subplots(ncols=2)
     axs[0].format(aspect=1.5)
-
-    fig, axs = pplt.subplots(ncols=2, proj=('cart', 'cyl'), aspect=2)
+    fig, axs = pplt.subplots(ncols=2, proj=("cart", "cyl"), aspect=2)
     axs[0].set_aspect(1)
+    return fig
 
 
-if pplt.internals._version_mpl <= '3.2':
-    @pplt.tests.image_compare
+if pplt.internals._version_mpl <= "3.2":
+
+    @pytest.mark.mpl_image_compare
     def test_basemap_labels():
         """
         Add basemap labels.
         """
-        fig, axs = pplt.subplots(ncols=2, proj='robin', refwidth=3, basemap=True)
-        axs.format(coast=True, labels='rt')
+        fig, axs = pplt.subplots(ncols=2, proj="robin", refwidth=3, basemap=True)
+        axs.format(coast=True, labels="rt")
+        return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_cartopy_labels():
     """
     Add cartopy labels.
     """
-    fig, axs = pplt.subplots(ncols=2, proj='robin', refwidth=3)
+    fig, axs = pplt.subplots(ncols=2, proj="robin", refwidth=3)
     axs.format(coast=True, labels=True)
     axs[0].format(inlinelabels=True)
     axs[1].format(rotatelabels=True)
+    return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_cartopy_contours():
     """
     Test bug with cartopy contours. Sometimes results in global coverage
@@ -58,82 +61,78 @@ def test_cartopy_contours():
     y = np.linspace(-90, 90, N)
     z = state.rand(N, N) * 10 - 5
     m = ax.contourf(
-        x, y, z,
+        x,
+        y,
+        z,
         transform=ccrs.PlateCarree(),
-        cmap='RdBu_r',
+        cmap="RdBu_r",
         vmin=-5,
         vmax=5,
     )
     fig.colorbar(m, ax=ax)
-
     fig = pplt.figure()
-    ax = fig.add_subplot(
-        projection=pplt.Mollweide(),
-        extent='auto'
-    )
+    ax = fig.add_subplot(projection=pplt.Mollweide(), extent="auto")
     ax.coastlines()
     N = 10
     m = ax.contourf(
         np.linspace(0, 180, N),
         np.linspace(0, 90, N)[1::2],
         state.rand(N // 2, N) * 10 + 5,
-        cmap='BuRd',
+        cmap="BuRd",
         transform=pplt.PlateCarree(),
-        edgefix=False
+        edgefix=False,
     )
     fig.colorbar(m, ax=ax)
+    return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_cartopy_manual():
     """
     Test alternative workflow without classes.
     """
     fig = pplt.figure()
-    proj = pplt.Proj('npstere')
+    proj = pplt.Proj("npstere")
     # fig.add_axes([0.1, 0.1, 0.9, 0.9], proj='geo', map_projection=proj)
-    fig.add_subplot(111, proj='geo', land=True, map_projection=proj)
+    fig.add_subplot(111, proj="geo", land=True, map_projection=proj)
+    return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_three_axes():
     """
     Test basic 3D axes here.
     """
-    pplt.rc['tick.minor'] = False
-    fig, ax = pplt.subplots(proj='3d', outerpad=3)
+    pplt.rc["tick.minor"] = False
+    fig, ax = pplt.subplots(proj="3d", outerpad=3)
+    return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_projection_dicts():
     """
     Test projection dictionaries.
     """
     fig = pplt.figure(refnum=1)
-    a = [
-        [1, 0],
-        [1, 4],
-        [2, 4],
-        [2, 4],
-        [3, 4],
-        [3, 0]
-    ]
-    fig.subplots(a, proj={1: 'cyl', 2: 'cart', 3: 'cart', 4: 'cart'})
+    a = [[1, 0], [1, 4], [2, 4], [2, 4], [3, 4], [3, 0]]
+    fig.subplots(a, proj={1: "cyl", 2: "cart", 3: "cart", 4: "cart"})
+    return fig
 
 
-@pplt.tests.image_compare
+@pytest.mark.mpl_image_compare
 def test_polar_projections():
     """
     Rigorously test polar features here.
     """
-    fig, ax = pplt.subplots(proj='polar')
+    fig, ax = pplt.subplots(proj="polar")
     ax.format(
         rlabelpos=45,
         thetadir=-1,
         thetalines=90,
         thetalim=(0, 270),
-        theta0='N',
+        theta0="N",
         r0=0,
         rlim=(0.5, 1),
         rlines=0.25,
     )
+    return fig
